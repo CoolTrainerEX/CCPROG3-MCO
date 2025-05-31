4a38af3f3d1f47afa741378c3f74cd9dee01ad20
CoolTrainerEX <92789937+CoolTrainerEX@users.noreply.github.com>
2025-05-31 16:16:45 +0800

    yes

---
+++ src/SpecialCoffeeTruck.java
@@ -0,0 +1,11 @@
+public class SpecialCoffeeTruck extends CoffeeTruck {
+    protected SpecialStorageBin[] specialStorageBins = new SpecialStorageBin[2];
+
+    private class SpecialStorageBin extends StorageBin {
+
+    }
+    
+    private enum SyrupIngredient {
+
+    }
+}
