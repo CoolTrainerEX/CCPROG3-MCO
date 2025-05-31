40264b3aa72db4794ed3c37b7a162e16caa2b2cc
CoolTrainerEX <92789937+CoolTrainerEX@users.noreply.github.com>
2025-05-31 16:16:43 +0800

    yes

---
+++ src/CoffeeTruck.java
@@ -0,0 +1,12 @@
+public class CoffeeTruck {
+    protected StorageBin[] storageBins = new StorageBin[8];
+    protected Location location;
+
+    protected class StorageBin {
+
+    }
+
+    protected enum Ingredient {
+
+    }
+}
